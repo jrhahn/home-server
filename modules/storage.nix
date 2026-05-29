688a5363b29fc8392b0e098629e7c62ee4a6f7af
Jürgen Hahn <mail.jhahn@gmail.com>
2026-05-29 12:18:55 +0200

    Update permissions for seafile-redis directory to enhance security

--- modules/storage.nix
+++ modules/storage.nix
@@ -26,7 +26,7 @@
     "f /srv/immich-originals/upload/.immich 0600 immich immich -"
     "d /srv/seafile 0750 root root -"
     "d /srv/seafile-mysql 0750 root root -"
-    "d /srv/seafile-redis 0750 root root -"
+    "d /srv/seafile-redis 0770 999 999 -"
     "d /srv/backups 0750 root root -"
     "d /srv/backups/database-dumps 0750 root root -"
   ];
